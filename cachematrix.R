ef77077158cc7e4a50adda06dbff38b18ec834d2
Joslynn Smith <joslynn_smith@yahoo.com>
2015-03-20 08:19:14 -0700

    Joslynn Smith
    Assignment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Joslynn Smith
+## cashing and calculating the inverse of a matrix
 
-## Write a short comment describing this function
+## function makeCashMatrix cashes the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y){
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) m <<- inverse
+	getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calulates the inverse of the matrix in the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data)
+	x$setinverse(m)
+	m
+}
+	
+
 }
